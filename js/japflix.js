fc0cfca9dff0de0e6132cb64e641273fcac0a666
ignaciocalc <ignaciocal13@gmail.com>
2025-09-17 05:46:40 -0300

    actividad terminada

--- js/japflix.js
+++ js/japflix.js
@@ -4,13 +4,43 @@ const
    buscar = document.getElementById("btnBuscar"),
    inputBuscar = document.getElementById("inputBuscar");
 
+const
+   tituloContSup = document.getElementById("offcanvasTopLabel"),
+   descContSup = document.getElementById("descripcion"),
+   catContSup = document.getElementById("categorias"),
+
+   year = document.getElementById("year"),
+   runtime = document.getElementById("runtime"),
+   budget = document.getElementById("budget"),
+   Revenue = document.getElementById("revenue");
+
 let 
    movies = [];
-
 
 fetch(linkMovies)
    .then(response => response.json())
    .then(response => movies = response);
+
+function contSuperior() {
+   const 
+      pelicula = JSON.parse(this.getAttribute('datosPeli'));
+   let
+      generos = pelicula.genres[0].name;
+   
+   for (let i = 1; i < pelicula.genres.length; i++) {
+      generos = generos + ' - ' + pelicula.genres[i].name;
+   }
+
+   tituloContSup.textContent = pelicula.title;
+   descContSup.textContent = pelicula.overview;
+   catContSup.textContent = generos;
+
+   // asignacion a boton more
+   year.textContent = pelicula.release_date.slice(0, 4);
+   runtime.textContent = pelicula.runtime + " min";
+   budget.textContent = "$" + pelicula.budget;
+   Revenue.textContent = "$" + pelicula.revenue;
+}
 
 buscar.addEventListener("click", function(){
    const 
@@ -58,7 +88,15 @@ buscar.addEventListener("click", function(){
          p.textContent = pelicula.tagline;
          
          li.appendChild(h2);
-         li.appendChild(p)
+         li.appendChild(p);
+
+         li.setAttribute('datosPeli', JSON.stringify(pelicula))
+         li.setAttribute("data-bs-toggle", "offcanvas");
+         li.setAttribute("data-bs-target", "#offcanvasTop");
+         li.setAttribute("aria-controls", "offcanvasTop");
+
+         li.addEventListener('click', contSuperior)
+
          lista.appendChild(li)
       });   
    }
