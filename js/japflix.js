aabd0b53c21f6a54cfda8813dda8929a311294ad
ignaciocalc <ignaciocal13@gmail.com>
2025-09-17 03:15:24 -0300

    parte 1 y 2 completada

--- js/japflix.js
+++ js/japflix.js
@@ -25,21 +25,42 @@ buscar.addEventListener("click", function(){
    }
    
    function mostrarElementos(resultBusqueda){
-
       resultBusqueda.forEach(pelicula => {
          const
             li = document.createElement('li'),
-            h2 = document.createElement('h2'),
+            h2 = document.createElement('h1'),
             p = document.createElement('p'),
-            span = document.createElement('span');
-         
+            estrellas = document.createElement('span'),
+            estrellasVoto = Math.trunc(pelicula.vote_average/2); 
 
          h2.textContent = pelicula.title;
-         h2.appendChild(span);
+         estrellas.className = "contEstrellas"
+         h2.appendChild(estrellas);
+
+         //crear estrellas marcadas
+         for (let i = 1; i <= estrellasVoto; i++) {
+            const
+               estrella = document.createElement('span');
+            
+            estrella.className = "fa fa-star checked";
+            estrellas.appendChild(estrella);
+         }
+
+         //crear strellas faltantes
+         for (let i = 1; i <= 5 - estrellasVoto; i++) {
+            const
+               estrella = document.createElement('span');
+            
+            estrella.className = "fa fa-star";
+            estrellas.appendChild(estrella);
+         }
+
+         p.textContent = pelicula.tagline;
          
          li.appendChild(h2);
+         li.appendChild(p)
          lista.appendChild(li)
-      });
+      });   
    }
    
    if (!(busqueda == "")) {
@@ -49,7 +70,9 @@ buscar.addEventListener("click", function(){
                                              (checkGenero(a.genres, busqueda.toLowerCase()))            ||
                                              (a.tagline.toLowerCase().includes(busqueda.toLowerCase())) ||
                                              (a.overview.toLowerCase().includes(busqueda.toLowerCase())));
-
+      
+      lista.replaceChildren();
+      
       mostrarElementos(resultBusqueda);
    }
 })
